c996fbb11e6ea7efc05faeebfb48d3d9ad3f712f
DTGuy <tguy5837@gmail.com>
2022-11-26 22:03:21 -0500

    got server running and added models

---
+++ models/Thought.js
@@ -0,0 +1,64 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent thought_id
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: 'Please provide a valid reaction.',
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+            ref: 'User'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    }
+);
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'Please provide some text',
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            require: true,
+            ref: 'User'
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('THought', ThoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+
+const emailValidate = (email) => {
+    const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    return regex.test(email);
+};
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: "You need to provide a username!",
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: "You need to provide a username!",
+            unique: true,
+            validate: [emailValidate, "Please provide a valid email address."],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+// get total friend count for this user
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
